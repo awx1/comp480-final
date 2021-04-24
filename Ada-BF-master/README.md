e2aff28c1b76ce76d927f71527731b2e9fecb796
Alex Xiong <awx1@rice.edu>
2021-04-23 23:06:11 -0400

    start

---
+++ Ada-BF-master/README.md
@@ -0,0 +1,18 @@
+# ADAPTIVE LEARNED BLOOM FILTER (ADA-BF): EFFICIENT UTILIZATION OF THE CLASSIFIER
+
+The python files include the implementation of the Bloom filter, learned Bloom filter, Ada-BF and disjoint Ada-BF, and print the size of False Positives of the corresponding algorithm.
+
+**Input arguments**: 
+- `--data_path`: a csv file includes the items, scores and labels; `--size_of_Ada_BF`: size of Bloom filter;
+- (for learned Bloom filter) `--threshold_min` and `--threshold_max` provide the range of the score threshold (between `threshold_min` and `threshold_max`). Items with score larger than the threshold are identified as keys;
+- (for Ada-BF and disjoint Ada-BF) `--num_group_min` and `--num_group_max` give the range of number of groups to divide (range of *g*
+); `--c_min` and `--c_max` provide the range of *c* where *c=m_j/m_{j+1}*
+
+**Commands**:
+- Run Bloom filter: `python Bloom_filter.py --data_path ./Datasets/URL_data.csv --size_of_BF 200000`
+- Run learned Bloom filter: `python learned_Bloom_filter.py --data_path ./Datasets/URL_data.csv --size_of_LBF 200000  --threshold_min 0.5   --threshold_max 0.95`
+- Run Ada-BF: `python Ada-BF.py --data_path ./Datasets/URL_data.csv --size_of_Ada_BF 200000  --num_group_min 8  --num_group_max 12  --c_min 1.6  --c_max 2.5`
+- Run disjoint Ada-BF: `python disjoint_Ada-BF.py --data_path ./Datasets/URL_data.csv --size_of_Ada_BF 200000  --num_group_min 8  --num_group_max 12  --c_min 1.6  --c_max 2.5`
+
+
+
---
+++ Ada-BF-master/learned_Bloom_filter.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+import argparse
+from Bloom_filter import BloomFilter
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_path', action="store", dest="data_path", type=str, required=True,
+                    help="path of the dataset")
+parser.add_argument('--threshold_min', action="store", dest="min_thres", type=float, required=True,
+                    help="Minimum threshold for positive samples")
+parser.add_argument('--threshold_max', action="store", dest="max_thres", type=float, required=True,
+                    help="Maximum threshold for positive samples")
+parser.add_argument('--size_of_LBF', action="store", dest="R_sum", type=int, required=True,
+                    help="size of the LBF")
+
+
+
+
+results = parser.parse_args()
+DATA_PATH = results.data_path
+min_thres = results.min_thres
+max_thres = results.max_thres
+R_sum = results.R_sum
+
+
+'''
+Load the data and select training data
+'''
+data = pd.read_csv(DATA_PATH)
+negative_sample = data.loc[(data['label']==-1)]
+positive_sample = data.loc[(data['label']==1)]
+train_negative = negative_sample.sample(frac = 0.3)
+
+
+def Find_Optimal_Parameters(max_thres, min_thres, R_sum, train_negative, positive_sample):
+    FP_opt = train_negative.shape[0]
+
+    for threshold in np.arange(min_thres, max_thres+10**(-6), 0.01):
+        query = positive_sample.loc[(positive_sample['score'] <= threshold),'query']
+        n = len(query)
+        bloom_filter = BloomFilter(n, R_sum)
+        bloom_filter.insert(query)
+        ML_positive = train_negative.loc[(train_negative['score'] > threshold),'query']
+        bloom_negative = train_negative.loc[(train_negative['score'] <= threshold),'query']
+        BF_positive = bloom_filter.test(bloom_negative, single_key=False)
+        FP_items = sum(BF_positive) + len(ML_positive)
+        print('Threshold: %f, False positive items: %d' %(round(threshold, 2), FP_items))
+        if FP_opt > FP_items:
+            FP_opt = FP_items
+            thres_opt = threshold
+            bloom_filter_opt = bloom_filter
+    return bloom_filter_opt, thres_opt
+
+
+
+
+'''
+Implement learned Bloom filter
+'''
+if __name__ == '__main__':
+    '''Stage 1: Find the hyper-parameters (spare 30% samples to find the parameters)'''
+    bloom_filter_opt, thres_opt = Find_Optimal_Parameters(max_thres, min_thres, R_sum, train_negative, positive_sample)
+
+    '''Stage 2: Run LBF on all the samples'''
+    ### Test queries
+    ML_positive = negative_sample.loc[(negative_sample['score'] > thres_opt), 'query']
+    bloom_negative = negative_sample.loc[(negative_sample['score'] <= thres_opt), 'query']
+    score_negative = negative_sample.loc[(negative_sample['score'] < thres_opt), 'score']
+    BF_positive = bloom_filter_opt.test(bloom_negative, single_key = False)
+    FP_items = sum(BF_positive) + len(ML_positive)
+    FPR = FP_items/len(negative_sample)
+    print('False positive items: {}; FPR: {}; Size of quries: {}'.format(FP_items, FPR, len(negative_sample)))
